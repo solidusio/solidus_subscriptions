7d7d2aaccc8eee8dd4e75e63db7b3e2271a2af73
Brendan Deere <brendan@stembolt.com>
2016-08-25 10:16:28 -0700

    Add Shoulda mathers and create a rails_helper

--- solidus_subscriptions.gemspec
+++ solidus_subscriptions.gemspec
@@ -30,4 +30,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'simplecov'
   s.add_development_dependency 'sqlite3'
   s.add_development_dependency 'pry'
+  s.add_development_dependency 'shoulda-matchers', '~> 3.1'
 end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,43 @@
+# Run Coverage report
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec/dummy'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Models', 'app/models'
+  add_group 'Views', 'app/views'
+  add_group 'Libraries', 'lib'
+end
+
+# Configure Rails Environment
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../dummy/config/environment.rb', __FILE__)
+
+require 'rspec/rails'
+require 'database_cleaner'
+require 'ffaker'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
+# Requires factories and other useful helpers defined in spree_core.
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/capybara_ext'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/factories'
+require 'spree/testing_support/url_helpers'
+
+# Requires factories defined in lib/solidus_subscriptions/factories.rb
+require 'solidus_subscriptions/factories'
+
+require 'spec_helper'
+require 'shoulda-matchers'
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
