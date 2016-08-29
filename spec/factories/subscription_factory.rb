3aa631dd889e7e320767a144321e22e5e037ce06
Brendan Deere <brendan@stembolt.com>
2016-08-29 16:15:28 -0700

    Subscription with_line_item factory
    A factory trait for building subscriptions with an associated
    subscription_line_item

--- spec/factories/subscription_factory.rb
+++ spec/factories/subscription_factory.rb
@@ -1,4 +1,14 @@
 FactoryGirl.define do
   factory :subscription, class: 'SolidusSubscriptions::Subscription' do
     user
+
+    trait :with_line_item do
+      transient do
+        line_item_traits []
+        test 9
+      end
+
+      line_item { build :subscription_line_item, *line_item_traits }
+    end
+  end
 end
