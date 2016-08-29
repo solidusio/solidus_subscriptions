1a02ab4352c086e41521a6cdfac7e737d619eb14
Brendan Deere <brendan@stembolt.com>
2016-08-29 14:18:12 -0700

    Spree::Orders have_many subscription_line_items
    Spree::Orders may contain many subscription_line_items. When the order is
    finalized these subscription_line_items are converted into subscritpions.
    The order needs to be able to get a list of associated subscription_line_items
    to be able to populate the full subscriptions.

---
+++ spec/overrides/spree/orders/subscription_line_items_association_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Spree::Orders::SubscriptionLineItemsAssociation, type: :model do
+  subject { Spree::Order.new }
+
+  it { is_expected.to have_many :subscription_line_items }
+end
