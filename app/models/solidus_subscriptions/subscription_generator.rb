19160a821ce79700ba7dbe4f6b09117ff0ea87da
Brendan Deere <brendan@stembolt.com>
2016-08-30 16:07:30 -0700

    Modify spree factories to have subscription helpers
    Build some spree resources with associated subscription models. Useful
    for testing

--- app/models/solidus_subscriptions/subscription_generator.rb
+++ app/models/solidus_subscriptions/subscription_generator.rb
@@ -11,7 +11,7 @@ module SolidusSubscriptions
     def self.activate(subscription_line_items)
       subscription_line_items.map do |subscription_line_item|
         user = subscription_line_item.order.user
-        Subscription.create(user: user, line_item: subscription_line_item)
+        Subscription.create!(user: user, line_item: subscription_line_item)
       end
     end
   end
---
+++ spec/factories/spree/order_factory.rb
@@ -0,0 +1,18 @@
+FactoryGirl.modify do
+  factory :order do
+    trait :with_subscription_line_items do
+      transient do
+        n_line_items 1
+      end
+
+      line_items do
+        build_list(
+          :line_item,
+          n_line_items,
+          :with_subscription_line_items,
+          order: @instance
+        )
+      end
+    end
+  end
+end
