3cedf9e7fa3889e815da021f7a38f377d7f15852
Brendan Deere <brendan@stembolt.com>
2016-09-01 14:01:15 -0700

    Installments can create a Builder object
    The Builder object knows how to take a subsctiption line item and
    generate a spree line item from it. It is used in populating orders

--- app/models/solidus_subscriptions/installment.rb
+++ app/models/solidus_subscriptions/installment.rb
@@ -12,5 +12,14 @@ module SolidusSubscriptions
     )
 
     validates :subscription, presence: true
+
+    # Get the builder for the subscription_line_item. This will be an
+    # object that can generate the appropriate line item for the subscribable
+    # object
+    #
+    # @return [SolidusSubscriptions::LineItemBuilder]
+    def line_item_builder
+      LineItemBuilder.new(subscription.line_item)
+    end
   end
 end
