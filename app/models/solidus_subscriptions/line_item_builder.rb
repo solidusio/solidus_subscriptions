91ffec775134461d939020788e3614cacd687250
Brendan Deere <brendan@stembolt.com>
2016-09-01 11:09:43 -0700

    Add LineItemBuilder
    This class is responsible for taking a SubscriptionLineItem and building
    it into a Spree::LineItem which can be added to an order

---
+++ app/models/solidus_subscriptions/line_item_builder.rb
@@ -0,0 +1,42 @@
+# This class is responsible for taking a SubscriptionLineItem and building
+# it into a Spree::LineItem which can be added to an order
+module SolidusSubscriptions
+  class LineItemBuilder
+    attr_reader :subscription_line_item
+
+    class UnsubscribableError < StandardError
+      def initialize(subscribable)
+        @subscribable = subscribable
+        super
+      end
+
+      def to_s
+        <<-MSG.squish
+          #{@subscribable.class} with id: #{@subscribable.id} cannot be
+          subscribed to.
+        MSG
+      end
+    end
+
+    # Get a new instance of a LineItemBuilder
+    #
+    # @params [SolidusSubscriptions::LineItem] :subscription_line_item, The
+    #   subscription line item to be converted into a Spree::LineItem
+    #
+    # @return [SolidusSubscriptions::LineItemBuilder]
+    def initialize(subscription_line_item)
+      @subscription_line_item = subscription_line_item
+    end
+
+    # Get a new (unpersisted) Spree::LineItem which matches the details of
+    # :subscription_line_item
+    #
+    # @return [Spree::LineItem]
+    def line_item
+      variant = Spree::Variant.find(subscription_line_item.subscribable_id)
+      raise UnsubscribableError.new(variant) unless variant.subscribable?
+
+      Spree::LineItem.new(variant: variant, quantity: subscription_line_item.quantity)
+    end
+  end
+end
