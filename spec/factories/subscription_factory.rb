f8afd40fa603c05ee2978b51753b5ab1add0add9
Brendan Deere <brendan@stembolt.com>
2016-09-06 15:19:31 -0700

    Ensure subscription_line_items have a completed order
    if a SubscriptionLineItem has an associated subscription, it should also
    have an associated completed order. This simplifies test setup greatly

--- spec/factories/subscription_factory.rb
+++ spec/factories/subscription_factory.rb
@@ -7,7 +7,18 @@ FactoryGirl.define do
         line_item_traits []
       end
 
-      line_item { build :subscription_line_item, *line_item_traits }
+      line_item do
+        order = create(:completed_order_with_pending_payment)
+
+        # Ensure the line item traits has an associated order
+        if line_item_traits.last.is_a? Hash
+          line_item_traits.last.reverse_merge!(order: order)
+        else
+          line_item_traits << { order: order }
+        end
+
+        build :subscription_line_item, *line_item_traits
+      end
     end
   end
 end
