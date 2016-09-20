4acc4bf7b64938fdc72e0643d95c5ddbfe5966de
Brendan Deere <brendan@stembolt.com>
2016-09-20 13:51:53 -0700

    Fix migration foreign keys
    This slipped through because I was using mysql locally. The foreign keys
    need to reference the complete table name and so cannot be inferred from
    the short column names

--- db/migrate/20160825202248_create_solidus_subscriptions_installments.rb
+++ db/migrate/20160825202248_create_solidus_subscriptions_installments.rb
@@ -1,11 +1,23 @@
 class CreateSolidusSubscriptionsInstallments < ActiveRecord::Migration
   def change
     create_table :solidus_subscriptions_installments do |t|
-      t.references :subscription, index: true, foreign_key: true, null: false
-      t.references :order, index: true, foreign_key: true
+      t.references :subscription, index: true, null: false
+      t.references :order, index: true
       t.date :actionable_date
 
       t.timestamps null: false
     end
+
+    add_foreign_key(
+      :solidus_subscriptions_installments,
+      :solidus_subscriptions_subscriptions,
+      column: :subscription_id
+    )
+
+    add_foreign_key(
+      :solidus_subscriptions_installments,
+      :spree_orders,
+      column: :order_id
+    )
   end
 end
