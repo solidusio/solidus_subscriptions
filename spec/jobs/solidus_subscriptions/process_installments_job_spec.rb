9050d2aab63da6afabb8ef354ba984c4a6f846d9
Brendan Deere <brendan@stembolt.com>
2016-09-23 15:04:11 -0700

    Add ProcessInstallmentsJob
    This job is responsible for creating a consolidated installment from a
    list of installments and processing it.

---
+++ spec/jobs/solidus_subscriptions/process_installments_job_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe SolidusSubscriptions::ProcessInstallmentsJob do
+  let(:root_order) { create :completed_order_with_pending_payment }
+  let(:installments) do
+    traits = {
+      subscription_traits: [{
+        line_item_traits: [{
+          spree_line_item: root_order.line_items.first
+        }]
+      }]
+    }
+
+    create_list(:installment, 2, traits)
+  end
+
+  describe '#perform' do
+    subject { described_class.new.perform(installments) }
+
+    it 'processes the consolidated installment' do
+      expect_any_instance_of(SolidusSubscriptions::ConsolidatedInstallment).
+        to receive(:process).once
+
+      subject
+    end
+  end
+end
