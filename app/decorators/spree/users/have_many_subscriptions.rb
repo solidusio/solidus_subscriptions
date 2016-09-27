e62b6d68d446b4d05085b695b81e3d5101677164
Brendan Deere <brendan@stembolt.com>
2016-09-26 17:23:38 -0700

    Users accept nested attributes for subs

--- app/decorators/spree/users/have_many_subscriptions.rb
+++ app/decorators/spree/users/have_many_subscriptions.rb
@@ -8,9 +8,24 @@ module Spree
           class_name: 'SolidusSubscriptions::Subscription',
           foreign_key: 'user_id'
         )
+
+        base.accepts_nested_attributes_for :subscriptions
       end
     end
   end
 end
 
 Spree.user_class.prepend(Spree::Users::HaveManySubscritptions)
+
+user_attributes = Spree::PermittedAttributes.user_attributes
+
+subscription_attributes = {
+  subscriptions_attributes: {
+    line_item_attributes: SolidusSubscriptions::Config.subscription_line_item_attributes
+  }
+}
+
+Spree::PermittedAttributes.class_variable_set(
+  '@@user_attributes',
+  user_attributes << subscription_attributes
+)
