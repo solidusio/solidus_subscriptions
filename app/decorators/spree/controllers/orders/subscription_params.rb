0931216cbc83f9a7a8838828a60cbc04e8afeea2
Brendan Deere <brendan@stembolt.com>
2016-09-27 14:56:25 -0700

    Override Spree Permitted attributes together
    Instead of overriding Spree::PermittedAttributes in various decorators,
    group this override together in its own module.
    
    At least now this is all happening in the same place

--- app/decorators/spree/controllers/orders/subscription_params.rb
+++ app/decorators/spree/controllers/orders/subscription_params.rb
@@ -16,15 +16,3 @@ module Spree
 end
 
 Spree::OrdersController.prepend(Spree::Controllers::Orders::SubscriptionParams)
-
-# Allow spree line items to accept nested attributes for subscritption line items
-line_item_attributes = Spree::PermittedAttributes.line_item_attributes
-
-subscription_line_item_attributes = {
-  subscription_line_items_attributes: SolidusSubscriptions::Config.subscription_line_item_attributes
-}
-
-Spree::PermittedAttributes.class_variable_set(
-  '@@line_item_attributes',
-  line_item_attributes << subscription_line_item_attributes
-)
