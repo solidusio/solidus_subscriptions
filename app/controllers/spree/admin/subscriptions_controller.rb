a8ec755d7393d7ef2dac9aed657663b2b5b2d376
Alex Blackie <alex@alexblackie.com>
2016-10-05 15:10:09 -0700

    Add subscription line item nested form

--- app/controllers/spree/admin/subscriptions_controller.rb
+++ app/controllers/spree/admin/subscriptions_controller.rb
@@ -14,6 +14,7 @@ module Spree
 
       def new
         @subscription = SolidusSubscriptions::Subscription.new
+        @line_item = SolidusSubscriptions::LineItem.new
       end
 
       def create
