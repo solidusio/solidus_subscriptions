ce88e34ae2c93c1d8f1912c6f12078d76e819ab5
Alex Blackie <alex@alexblackie.com>
2016-10-11 10:28:00 -0700

    Begin spec for variant pretty names

---
+++ app/decorators/spree/variant_pretty_name.rb
@@ -0,0 +1,13 @@
+module Spree
+  module Variants
+    module VariantPrettyName
+      def pretty_name
+        name = "#{product.name}"
+        name += " - #{options_text}" if options_text.present?
+        name
+      end
+    end
+  end
+end
+
+Spree::Variant.prepend Spree::Variants::VariantPrettyName
---
+++ spec/models/spree/variant_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+
+RSpec.describe Spree::Variant, type: :model do
+  describe "#pretty_name" do
+  end
+end
