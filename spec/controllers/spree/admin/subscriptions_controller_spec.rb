3d47275ac7073b251bc6595f4e3f3403422e829e
Brendan Deere <brendan@stembolt.com>
2017-07-24 14:09:30 -0700

    Refactor Admin Subs Controller spec
    Make this spec a controller spec so that the logic in the templates is
    actually run. This spec would have caught the bug which broke the
    ability to update subscriptions from the admin area if it had been
    present when the switch from subscriptions having one line item to them
    having many line items

--- spec/controllers/spree/admin/subscriptions_controller_spec.rb
+++ spec/controllers/spree/admin/subscriptions_controller_spec.rb
@@ -1,40 +1,49 @@
 require 'rails_helper'
-RSpec.describe Spree::Admin::SubscriptionsController, type: :controller do
-  routes { Spree::Core::Engine.routes }
+RSpec.describe Spree::Admin::SubscriptionsController, type: :request do
+  extend Spree::TestingSupport::AuthorizationHelpers::Request
   stub_authorization!
 
   describe 'get /admin/subscriptions' do
-    subject { get :index }
+    subject do
+      get spree.admin_subscriptions_path
+      response
+    end
 
     it { is_expected.to be_successful }
   end
 
   describe 'GET :new' do
-    subject { get :new }
+    subject do
+      get spree.new_admin_subscription_path
+      response
+    end
 
     it { is_expected.to be_successful }
   end
 
   describe 'GET :edit' do
-    subject { get :edit, params: { id: subscription.id } }
+    subject do
+      get spree.edit_admin_subscription_path(subscription)
+      response
+    end
+
     let(:subscription) { create :subscription, :actionable }
 
     it { is_expected.to be_successful }
   end
 
   describe 'PUT :update' do
-    subject { put :update, params: subscription_params }
+    subject { put spree.admin_subscription_path(subscription), params: subscription_params }
 
     let(:expected_date) { DateTime.parse('2001/11/12') }
     let(:subscription) { create :subscription, :actionable }
     let(:subscription_params) do
       {
-        id: subscription.id,
         subscription: { actionable_date: expected_date }
       }
     end
 
-    it { is_expected.to redirect_to admin_subscriptions_path }
+    it { is_expected.to redirect_to spree.admin_subscriptions_path }
 
     it 'updates the subscription attributes', :aggregate_failures do
       expect { subject }.to change { subscription.reload.actionable_date }.to expected_date
@@ -42,11 +51,11 @@ RSpec.describe Spree::Admin::SubscriptionsController, type: :controller do
   end
 
   describe 'POST cancel' do
-    subject { delete :cancel, params: { id: subscription.id } }
+    subject { delete spree.cancel_admin_subscription_path(subscription) }
     context 'the subscription can be canceled' do
       let(:subscription) { create :subscription, :actionable }
 
-      it { is_expected.to redirect_to admin_subscriptions_path }
+      it { is_expected.to redirect_to spree.admin_subscriptions_path }
 
       it 'has a message' do
         subject
@@ -61,7 +70,7 @@ RSpec.describe Spree::Admin::SubscriptionsController, type: :controller do
     context 'the subscription cannot be canceled' do
       let(:subscription) { create :subscription, :canceled }
 
-      it { is_expected.to redirect_to admin_subscriptions_path }
+      it { is_expected.to redirect_to spree.admin_subscriptions_path }
 
       it 'has a message' do
         subject
@@ -75,12 +84,12 @@ RSpec.describe Spree::Admin::SubscriptionsController, type: :controller do
   end
 
   describe 'POST activate' do
-    subject { post :activate, params: { id: subscription.id } }
+    subject { post spree.activate_admin_subscription_path(subscription) }
 
     context 'the subscription can be activated' do
       let(:subscription) { create :subscription, :canceled, :with_line_item }
 
-      it { is_expected.to redirect_to admin_subscriptions_path }
+      it { is_expected.to redirect_to spree.admin_subscriptions_path }
 
       it 'has a message' do
         subject
@@ -95,7 +104,7 @@ RSpec.describe Spree::Admin::SubscriptionsController, type: :controller do
     context 'the subscription cannot be activated' do
       let(:subscription) { create :subscription, :actionable, :with_line_item }
 
-      it { is_expected.to redirect_to admin_subscriptions_path }
+      it { is_expected.to redirect_to spree.admin_subscriptions_path }
 
       it 'has a message' do
         subject
@@ -109,12 +118,12 @@ RSpec.describe Spree::Admin::SubscriptionsController, type: :controller do
   end
 
   describe 'POST skip' do
-    subject { post :skip, params: { id: subscription.id } }
+    subject { post spree.skip_admin_subscription_path(subscription) }
 
     let(:subscription) { create :subscription, :actionable, :with_line_item }
     let!(:expected_date) { subscription.next_actionable_date }
 
-    it { is_expected.to redirect_to admin_subscriptions_path }
+    it { is_expected.to redirect_to spree.admin_subscriptions_path }
 
     it 'has a message' do
       subject
