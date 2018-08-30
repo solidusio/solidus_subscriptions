78cbeade07f7c3cfefbe9d348663d68ac88c5053
fastjames <fastjames@gmail.com>
2018-08-30 09:34:29 -0500

    Change static factory attrs to dynamic
    FactoryBot 5.0 will no longer support static factory attributes, so we
    need to update our factories in advance to silence the deprecation
    warnings.

--- lib/solidus_subscriptions/testing_support/factories/line_item_factory.rb
+++ lib/solidus_subscriptions/testing_support/factories/line_item_factory.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :subscription_line_item, class: 'SolidusSubscriptions::LineItem' do
     subscribable_id { create(:variant, subscribable: true).id }
-    quantity 1
-    interval_length 1
-    interval_units :month
+    quantity { 1 }
+    interval_length { 1 }
+    interval_units { :month }
 
     association :spree_line_item, factory: :line_item
 
     trait :with_subscription do
       transient do
-        subscription_traits []
+        subscription_traits { [] }
       end
 
       subscription { build :subscription, *subscription_traits }
--- lib/solidus_subscriptions/testing_support/factories/spree/line_item_factory.rb
+++ lib/solidus_subscriptions/testing_support/factories/spree/line_item_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.modify do
   factory :line_item do
     trait :with_subscription_line_items do
       transient do
-        n_subscription_line_items 1
+        n_subscription_line_items { 1 }
       end
 
       subscription_line_items do
