9b63f7c0eb224f0e13653c21659e56ed84ab1ba4
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-12 15:36:43 +0200

    Fix remove_foreign_key failing in Rails 6
    In Rails 6, this statement fails because Rails thinks the foreign
    key does not exist for some reason. Since the remove_foreign_key
    call is not needed because the column is removed in the subsequent
    statement, we can remove it altogether.

--- db/migrate/20161017175509_remove_order_id_from_solidus_subscriptions_installments.rb
+++ db/migrate/20161017175509_remove_order_id_from_solidus_subscriptions_installments.rb
@@ -1,6 +1,5 @@
 class RemoveOrderIdFromSolidusSubscriptionsInstallments < SolidusSupport::Migration[4.2]
   def change
-    remove_foreign_key :solidus_subscriptions_installments, column: :order_id
     remove_column :solidus_subscriptions_installments, :order_id, :integer
   end
 end
