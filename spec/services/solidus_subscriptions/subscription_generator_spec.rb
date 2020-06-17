aeac07481f55ccdb42d1f737e5108b684f5e18da
Alessandro Desantis <desa.alessandro@gmail.com>
2020-06-17 13:33:37 +0200

    Copy order billing address when generating subscriptions

--- spec/services/solidus_subscriptions/subscription_generator_spec.rb
+++ spec/services/solidus_subscriptions/subscription_generator_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe SolidusSubscriptions::SubscriptionGenerator do
       expect(subject).to have_attributes(
         user: user,
         shipping_address: subscription_line_item.spree_line_item.order.ship_address,
+        billing_address: subscription_line_item.spree_line_item.order.bill_address,
         interval_length: subscription_line_item.interval_length,
         interval_units: subscription_line_item.interval_units,
         end_date: subscription_line_item.end_date,
