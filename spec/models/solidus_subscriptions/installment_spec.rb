ac4a0160c6e5f81b8aeaa4466c4995fc84e62e26
Alessandro Desantis <desa.alessandro@gmail.com>
2020-08-27 09:10:21 +0200

    Remove ActiveRecord association tests
    Associations are configuration, not behavior, and they shouldn't be
    tested.

--- spec/models/solidus_subscriptions/installment_spec.rb
+++ spec/models/solidus_subscriptions/installment_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe SolidusSubscriptions::Installment, type: :model do
-  it { is_expected.to have_many :details }
-  it { is_expected.to belong_to :subscription }
-
   it { is_expected.to validate_presence_of :subscription }
 
   let(:installment) { create :installment }
