6614671689cc337422478fef87cf4703d241c539
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2020-09-17 17:38:58 +0200

    Add order association to subscriptions

---
+++ app/decorators/models/solidus_subscriptions/spree/order/subscription_association.rb
@@ -0,0 +1,13 @@
+module SolidusSubscriptions
+  module Spree
+    module Order
+      module SubscriptionAssociation
+        def self.prepended(base)
+          base.belongs_to :subscription, class_name: '::SolidusSubscriptions::Subscription', optional: true
+        end
+      end
+    end
+  end
+end
+
+Spree::Order.prepend(SolidusSubscriptions::Spree::Order::SubscriptionAssociation)
---
+++ db/migrate/20200917072152_add_subscription_reference_to_orders.rb
@@ -0,0 +1,5 @@
+class AddSubscriptionReferenceToOrders < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :spree_orders, :subscription, null: true, foreign_key: { to_table: :solidus_subscriptions_subscriptions }
+  end
+end
