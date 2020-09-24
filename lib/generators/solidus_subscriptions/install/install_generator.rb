11b1a59321f238a3a7fc8802e8f7316cee7d1e80
Alessandro Desantis <desa.alessandro@gmail.com>
2020-09-24 13:42:53 +0200

    Remove empty asset files

--- lib/generators/solidus_subscriptions/install/install_generator.rb
+++ lib/generators/solidus_subscriptions/install/install_generator.rb
@@ -6,13 +6,7 @@ module SolidusSubscriptions
       class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_subscriptions\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_subscriptions\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_subscriptions\n", before: %r{\*/}, verbose: true # rubocop:disable Metrics/LineLength
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_subscriptions\n", before: %r{\*/}, verbose: true
       end
 
       def add_migrations
