b4c1e0fccd009afe8d93772cb35b82a59519b231
Alessandro Desantis <desa.alessandro@gmail.com>
2020-09-25 13:31:18 +0200

    Use integer foreign keys for compatibility with old tables

--- db/migrate/20200917072152_add_subscription_reference_to_orders.rb
+++ db/migrate/20200917072152_add_subscription_reference_to_orders.rb
@@ -1,5 +1,8 @@
 class AddSubscriptionReferenceToOrders < ActiveRecord::Migration[5.2]
   def change
-    add_reference :spree_orders, :subscription, null: true, foreign_key: { to_table: :solidus_subscriptions_subscriptions }
+    add_reference :spree_orders, :subscription, null: true, foreign_key: {
+      to_table: :solidus_subscriptions_subscriptions,
+      type: :integer,
+    }
   end
 end
