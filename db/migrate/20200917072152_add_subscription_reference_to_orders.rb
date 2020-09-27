5b41665599018c7d9eccc5a2f2fcf9f00fbb82d9
Alessandro Desantis <desa.alessandro@gmail.com>
2020-09-27 11:29:02 +0200

    Fix foreign key type for MySQL

--- db/migrate/20200917072152_add_subscription_reference_to_orders.rb
+++ db/migrate/20200917072152_add_subscription_reference_to_orders.rb
@@ -1,8 +1,11 @@
 class AddSubscriptionReferenceToOrders < ActiveRecord::Migration[5.2]
   def change
-    add_reference :spree_orders, :subscription, null: true, foreign_key: {
-      to_table: :solidus_subscriptions_subscriptions,
+    add_reference(
+      :spree_orders,
+      :subscription,
+      null: true,
       type: :integer,
-    }
+      foreign_key: { to_table: :solidus_subscriptions_subscriptions }
+    )
   end
 end
