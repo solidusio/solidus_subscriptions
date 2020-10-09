d7369ba04b3657e47e21e6d24af516886ef0dbc4
Alessandro Desantis <desa.alessandro@gmail.com>
2020-10-09 14:04:49 +0200

    Fire events for subscription payment method updates

---
+++ spec/models/spree/wallet_payment_source_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe Spree::WalletPaymentSource do
+  describe 'setting it as the default' do
+    it 'reports a payment method changed event for subscriptions that use the default payment source' do
+      stub_const('Spree::Event', class_spy(Spree::Event))
+      user = create(:user)
+      subscription = create(:subscription, user: user)
+      payment_source = create(:credit_card, user: user)
+      wallet_payment_source = user.wallet.add(payment_source)
+
+      user.wallet.default_wallet_payment_source = wallet_payment_source
+
+      expect(Spree::Event).to have_received(:fire).with(
+        'solidus_subscriptions.subscription_payment_method_changed',
+        subscription: subscription,
+      )
+    end
+  end
+end
