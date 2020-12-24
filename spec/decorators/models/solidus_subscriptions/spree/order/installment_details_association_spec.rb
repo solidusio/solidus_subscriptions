f390b53b57d1a75bab9b7d059772fdb65883827b
Sean <seand7565@gmail.com>
2020-12-24 09:28:28 -0600

    Add Installment Details association to Order
    This is useful if you want to connect orders to installments - for
    instance, if you wanted to get a list of orders for a user that
    are part of an installment.

---
+++ spec/decorators/models/solidus_subscriptions/spree/order/installment_details_association_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+RSpec.describe SolidusSubscriptions::Spree::Order::InstallmentDetailsAssociation, type: :model do
+  subject { Spree::Order.new }
+
+  it { is_expected.to have_many :installment_details }
+end
