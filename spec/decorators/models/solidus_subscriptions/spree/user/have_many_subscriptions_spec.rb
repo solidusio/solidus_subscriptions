b904fb5aa324b0907cba9bdd3c718ae9d704c536
luca-landa <lucalanda@hotmail.it>
2021-01-29 14:48:07 +0100

    Deprecate User subscription nested attributes

--- spec/decorators/models/solidus_subscriptions/spree/user/have_many_subscriptions_spec.rb
+++ spec/decorators/models/solidus_subscriptions/spree/user/have_many_subscriptions_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe SolidusSubscriptions::Spree::User::HaveManySubscriptions, type: :
 
   it { is_expected.to have_many :subscriptions }
   it { is_expected.to accept_nested_attributes_for :subscriptions }
+
+  describe '#subscriptions_attributes=' do
+    it 'throws a deprecation warning' do
+      allow(::Spree::Deprecation).to receive(:warn)
+
+      subject.subscriptions_attributes = [{ interval_length: 2 }]
+
+      expect(::Spree::Deprecation)
+        .to have_received(:warn)
+        .with(/Creating or updating subscriptions through Spree::User nested attributes is deprecated/)
+    end
+  end
 end
