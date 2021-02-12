d97bba60a4b9e56e549de0fecf7c83b2b6f0a093
luca-landa <lucalanda@hotmail.it>
2021-02-12 15:06:47 +0100

    Remove variant from subscriptions on soft deletion

---
+++ spec/decorators/models/solidus_subscriptions/spree/variant/auto_delete_from_subscriptions_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe SolidusSubscriptions::Spree::Variant::AutoDeleteFromSubscriptions, type: :model do
+  subject { create(:variant, subscribable: true) }
+
+  describe '.discard' do
+    it 'deletes itself from subscriptions' do
+      subscription = create(:subscription)
+      create(:subscription_line_item, subscription: subscription, subscribable: subject)
+
+      expect { subject.discard }.to change { SolidusSubscriptions::LineItem.count }.by(-1)
+    end
+  end
+
+  describe '.destroy' do
+    it 'deletes itself from subscriptions' do
+      subscription = create(:subscription)
+      create(:subscription_line_item, subscription: subscription, subscribable: subject)
+
+      expect { subject.destroy }.to change { SolidusSubscriptions::LineItem.count }.by(-1)
+    end
+  end
+end
