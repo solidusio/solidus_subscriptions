a72dd28a22e38fcded63207cd5a5e3c0a3ebdf9e
Elia Schito <elia@schito.me>
2021-03-11 13:02:52 +0100

    Update permitted action names for Solidus v2.11
    Under v2.11 :display won't be enough anymore for :show controller
    actions. The latter needs to be stated explicitly.

--- lib/solidus_subscriptions/permission_sets/default_customer.rb
+++ lib/solidus_subscriptions/permission_sets/default_customer.rb
@@ -4,12 +4,12 @@ module SolidusSubscriptions
   module PermissionSets
     class DefaultCustomer < ::Spree::PermissionSets::Base
       def activate!
-        can [:display, :update, :skip, :cancel], Subscription, ['user_id = ?', user.id] do |subscription, guest_token|
+        can [:show, :display, :update, :skip, :cancel], Subscription, ['user_id = ?', user.id] do |subscription, guest_token|
           (subscription.guest_token.present? && subscription.guest_token == guest_token) ||
             (subscription.user && subscription.user == user)
         end
 
-        can [:display, :update, :destroy], LineItem do |line_item, guest_token|
+        can [:show, :display, :update, :destroy], LineItem do |line_item, guest_token|
           (line_item.subscription&.guest_token.present? && line_item.subscription.guest_token == guest_token) ||
             (line_item.subscription&.user && line_item.subscription.user == user)
         end
