4722a3ab8c0b03d96b420d59dd08adb1a111d3f0
Azeem838 <azeenmahmed101@gmail.com>
2021-05-11 16:28:23 +0200

    Handle multiple subscription line items being passed as params.

--- app/models/solidus_subscriptions/subscription_line_item_builder.rb
+++ app/models/solidus_subscriptions/subscription_line_item_builder.rb
@@ -3,9 +3,12 @@ module SolidusSubscriptions
     private
 
     def create_subscription_line_item(line_item)
-      SolidusSubscriptions::LineItem.create!(
-        subscription_params.merge(spree_line_item: line_item)
-      )
+      subscription_params[:subscribable_id].split(" ").each do |subscribable_id|
+        merged_params = subscription_params.merge(spree_line_item: line_item)
+        merged_params["subscribable_id"] = subscribable_id
+        SolidusSubscriptions::LineItem.create!(merged_params)
+      end
+
 
       # Rerun the promotion handler to pickup subscription promotions
       Spree::PromotionHandler::Cart.new(line_item.order).activate
