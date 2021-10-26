10210c61986f0980a3b9771f73b15f5834fb9de0
Sergey Pchelintsev <linz.sergey@gmail.com>
2021-10-26 20:29:32 +0300

    ADN-1523 fix disable_subscription_order_promotion_rule for recurring orders

--- app/models/solidus_subscriptions/disable_subscription_order_promotion_rule.rb
+++ app/models/solidus_subscriptions/disable_subscription_order_promotion_rule.rb
@@ -1,11 +1,15 @@
 module SolidusSubscriptions
   class DisableSubscriptionOrderPromotionRule < Spree::PromotionRule
     def applicable?(promotable)
-      promotable.is_a? Spree::Order
+      promotable.is_a?(Spree::Order)
     end
 
-    def eligible?(order, **_options)
-      order.subscription_line_items.none?
+    def eligible?(_order, **_options)
+      true
+    end
+
+    def actionable?(line_item)
+      !line_item.order.subscription_order? && line_item.subscription_line_items.none?
     end
   end
 end
