30c451bda1ab4ce3d6a212b85531ca7dc0991be2
Martin Meyerhoff <mamhoff@gmail.com>
2025-02-13 12:12:10 +0100

    Use current order helper instead of instance variable
    The API to get the current order in a controller is the helper, not the
    instance variable.

--- spec/controllers/concerns/create_subscription_spec.rb
+++ spec/controllers/concerns/create_subscription_spec.rb
@@ -5,11 +5,6 @@ RSpec.describe CreateSubscription, type: :controller do
   subject(:controller_instance) do
     Class.new(ApplicationController) do
       include CreateSubscription
-      attr_accessor :params, :current_order
-
-      def initialize(params = {})
-        super
-      end
     end.new
   end
 
@@ -17,7 +12,7 @@ RSpec.describe CreateSubscription, type: :controller do
   let(:order) { create(:order) }
 
   before do
-    controller_instance.current_order = order
+    allow(controller_instance).to receive(:current_order).and_return(order)
   end
 
   describe '#subscription_line_item_params_present?' do
@@ -49,15 +44,19 @@ RSpec.describe CreateSubscription, type: :controller do
   end
 
   describe '#handle_subscription_line_items' do
-    context 'when subscription params are missing' do
-      it 'does not invoke handle_subscription_line_items and does not create a subscription line item' do
-        order.line_items.count
+    before do
+      allow(controller_instance).to receive(:params).and_return(params)
+    end
 
-        controller_instance.params = {
+    context 'when subscription params are missing' do
+      let(:params) do
+        {
           variant_id: variant.id,
           subscription_line_item: {}
         }
+      end
 
+      it 'does not invoke handle_subscription_line_items and does not create a subscription line item' do
         expect(controller_instance.send(:valid_subscription_line_item_params?)).to be false
 
         expect(controller_instance).not_to receive(:handle_subscription_line_items)
@@ -67,10 +66,8 @@ RSpec.describe CreateSubscription, type: :controller do
     end
 
     context 'when subscription params are present' do
-      it 'calls create_subscription_line_item with the correct line item' do
-        line_item = create(:line_item, order: order, variant: variant)
-
-        controller_instance.params = {
+      let(:params) do
+        {
           variant_id: variant.id,
           subscription_line_item: {
             subscribable_id: 1,
@@ -78,6 +75,10 @@ RSpec.describe CreateSubscription, type: :controller do
             interval_length: 1
           }
         }
+      end
+
+      it 'calls create_subscription_line_item with the correct line item' do
+        line_item = create(:line_item, order: order, variant: variant)
 
         allow(order.line_items).to receive(:find_by).with(variant_id: variant.id).and_return(line_item)
 
